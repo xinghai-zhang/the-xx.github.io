435b83f211f7c2f2f67002b33ce080f285289751
the-xx <xinghai@umich.edu>
2015-07-27 16:07:38 -0400

    add slide

--- js/slide.js
+++ js/slide.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-      $('.slide').slick({
-       
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      adaptiveHeight: true
+ $(document).ready(function(){
+   $('.slide').slick({
     
-      });
-});
+   dots: true,
+   infinite: true,
+   speed: 300,
+   slidesToShow: 1,
+   adaptiveHeight: true
+ 
+   });
+ });
