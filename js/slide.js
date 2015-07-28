78924e42ae84930ff19fb7715097ee8bf0460824
the-xx <xinghai@umich.edu>
2015-07-27 23:43:35 -0400

    center mode

--- js/slide.js
+++ js/slide.js
@@ -2,8 +2,28 @@
    $('.slide').slick({
    dots: true,
    infinite: true,
-   speed: 500,
-   fade: true,
-   cssEase: 'linear',
+    centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
    });
  });
