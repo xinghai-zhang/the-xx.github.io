06fab351315aa8565395537ced9af57c31159d63
the-xx <xinghai@umich.edu>
2015-07-27 23:46:34 -0400

    align

--- js/slide.js
+++ js/slide.js
@@ -2,28 +2,8 @@
    $('.slide').slick({
    dots: true,
    infinite: true,
-    centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+   speed: 500,
+   fade: true,
+   cssEase: 'linear',
    });
  });
