4cd62595bf37e4bb5d87c35437881120f2b7aa28
the-xx <xinghai@umich.edu>
2015-07-29 14:13:16 -0400

    auto scroll

--- js/slide.js
+++ js/slide.js
@@ -1,11 +1,9 @@
  $(document).ready(function(){
    $('.slide').slick({
    dots: true,
-   infinite: true,
-   speed: 500,
-   fade: true,
-    slidesToShow: 1,
-   centerMode: true,
-   variableWidth: true
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 2000,
    });
  });
