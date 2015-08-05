af314712aecee3a88b147ab9bd1253bf3b6594f8
the-xx <xinghai@umich.edu>
2015-08-05 13:10:44 -0400

    go back to original, need list

--- js/donut1.js
+++ js/donut1.js
@@ -16,7 +16,7 @@ var svg = d3.select("#donut").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
-    .attr("transform", "translate(" + width + "," + height / 2 + ")");
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 d3.tsv("data/data.tsv", type, function(error, data) {
   var path = svg.datum(data).selectAll("path")
